8e058db452dab618688d177fb38fed9cd1cab3a6
natalia zhydejkina <Karina.Varlena@instantconnectnow.com>
2020-09-12 23:10:53 +0300

    lesson1

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        float f = 3.14f;
+        int i= 25;
+        int i2;
+        i2 = 2;
+        char c = 'c';
+        long l = 683968205;
+        byte b =127;
+        boolean bool = true;
+        short s = 1235;
+        String Str = "strbreberbep";
+
+        int sum = i+i2;
+        System.out.println(sum);
+        int diff =i-i2;
+        System.out.println(diff);
+        int ymn = i*i2;
+        System.out.println(ymn);
+        double dil = i/i2;
+        System.out.println(dil);
+
+        System.out.println("Hello World");
+
+        int i3 = 2005;
+        int i4 =7 ;
+        int i5 =12 ;
+
+        System.out.println(i3+i4+i5);
+
+    }
+}
